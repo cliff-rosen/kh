791a560cf0c9f7d1be3c228a1ae7ec6fcb928c53
Cliff Rosen <cliff.rosen@gmail.com>
2025-12-20 12:05:16 -0700

    'b'

--- frontend/src/lib/api/modelApi.ts
+++
@@ -1,9 +0,0 @@
-import { ModelData } from '../types/models';
-import { api } from './index';
-
-export const modelApi = {
-    getModels: async (): Promise<ModelData> => {
-        const response = await api.get('/api/llm/models');
-        return response.data;
-    }
-}; 
